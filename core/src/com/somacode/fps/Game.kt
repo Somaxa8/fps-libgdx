3ba55aaf6a54a0384200d0422711661d6a2919ee
somacode <silviofrancoxa8@gmail.com>
2021-08-23 00:35:52 -0500

    initial commit

---
+++ core/src/com/somacode/fps/Game.kt
@@ -0,0 +1,19 @@
+package com.somacode.fps
+
+import com.badlogic.gdx.ApplicationAdapter
+
+class Game : ApplicationAdapter() {
+
+    override fun create() {
+
+    }
+
+    override fun render() {
+
+    }
+
+    override fun dispose() {
+
+    }
+
+}
